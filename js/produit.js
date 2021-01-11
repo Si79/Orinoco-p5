b697b34916a675e473475630325033e46d93e123
Si79 <67465893+Si79@users.noreply.github.com>
2021-01-11 13:09:48 +0100

    récupération des images et affichage des prix

--- js/produit.js
+++ js/produit.js
@@ -2,5 +2,18 @@
 const url = window.location.search
 var searchParams = new URLSearchParams(url);
 
-const h1 = document.querySelector('h1')
-h1.innerHTML = searchParams.get('product')
+const titre = document.querySelector('h1')
+const image = document.querySelector('img')
+
+const xhr = new XMLHttpRequest(); // Création d'un objet XMLHttpRequest
+xhr.open('GET', 'https://oc-p5-api.herokuapp.com/api/teddies/' + searchParams.get('product'));
+xhr.send();
+
+xhr.onreadystatechange = function(){
+    if(xhr.status === 200 && xhr.readyState === 4){ //On s'assure que la requete est terminée et que la réponse est disponible
+        const product = JSON.parse(xhr.responseText) // On stock dans la variable products les produits récupérés de l'API
+        
+        titre.innerHTML = product.name
+        image.setAttribute('src', product.imageUrl)
+    }
+}
