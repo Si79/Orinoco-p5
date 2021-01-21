ec968c149fb721939f1e46c73bf2e4cd72824453
Si79 <67465893+Si79@users.noreply.github.com>
2021-01-21 22:44:16 +0100

    Ajout couleur et bouton select

--- js/produit.js
+++ js/produit.js
@@ -2,9 +2,13 @@
 const url = window.location.search
 var searchParams = new URLSearchParams(url);
 
-const titre = document.querySelector('h1')
-const image = document.querySelector('img')
+// ON récupère les éléments HTML
+const titre = document.querySelector('#title')
+const description = document.querySelector('#description')
+const image = document.querySelector('#image-produit')
+const couleurs = document.querySelector('#couleurs')
 
+//On appelle l'API pour récupérer les informations du produit
 const xhr = new XMLHttpRequest(); // Création d'un objet XMLHttpRequest
 xhr.open('GET', 'https://oc-p5-api.herokuapp.com/api/teddies/' + searchParams.get('product'));
 xhr.send();
@@ -14,7 +18,17 @@ xhr.onreadystatechange = function(){
         const product = JSON.parse(xhr.responseText) // On stock dans la variable products les produits récupérés de l'API
         
         titre.innerHTML = product.name
+        description.innerHTML = product.description
         image.setAttribute('src', product.imageUrl)
+
+        for(let i = 0; i < product.colors.length; i++){
+            const option = '<option value="' + product.colors[i] + '">' + product.colors[i] + '</option>'
+            console.log(option)
+            couleurs.innerHTML += option;
+        }
+
+
+        // Ajouter un évènement au click sur le bouton pour permettre l'ajout du produit dans le localStorage
     
     }
 
