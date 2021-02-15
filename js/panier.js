0d907d06cae994eff4d2690d8f5c8a21fc5f2f83
Si79 <67465893+Si79@users.noreply.github.com>
2021-02-15 13:00:13 +0100

    Création du panier et ajout de produits

--- js/panier.js
+++ js/panier.js
@@ -1,1 +1,24 @@
-// TODO: Récupérer le tableau du localStorage pour afficher le détail des produits qui y sont stockés
+let produitsAjoutPanier = JSON.parse(localStorage.getItem("panier"));
+
+let sectionPanier = document.createElement("section");
+
+if (produitsAjoutPanier !== null) {
+  sectionPanier.id = "sectionWithProduits";
+
+  let table = document.createElement("table");
+  for (let i in produitsAjoutPanier) {
+    table.innerHTML +=
+      "<tr><td>" +
+      produitsAjoutPanier[i].name +
+      "</td><td><img src=" +
+      produitsAjoutPanier[i].imageUrl +
+      " width=100px></td></tr>";
+      //prix, couleur
+  }
+  sectionPanier.appendChild(table);
+}
+document.body.appendChild(sectionPanier);
+//ajouter le total (somme de l'ens des produits du panier)
+
+//Perspectives: supprimer un elt du panier
+//formulaire en dessous du panier
