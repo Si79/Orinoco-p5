12ff39e03095b7672a04a6f1c8bc080cc70f8ecf
Si79 <67465893+Si79@users.noreply.github.com>
2021-04-21 00:08:46 +0200

    ajout panier

--- js/panier.js
+++ js/panier.js
@@ -1,43 +1,26 @@
+
 let produitsAjoutPanier = JSON.parse(localStorage.getItem("panier"));
 
-let titrePanier = document.createElement('h1');
-titrePanier.textContent = "Panier";
-
-document.getElementById('Panier').appendChild(titrePanier);
-
 let sectionPanier = document.createElement("section");
- 
+
 if (produitsAjoutPanier !== null) {
-  sectionPanier.id = "sectionProduits";
+  sectionPanier.id = "sectionWithProduits";
 
-  let sectionTitre = document.createElement('h2');
-
-  sectionTitre.textContent = "Produit ajouté";
-
-  sectionTitre.id = "sectionTitre";
-  
-//console.log(sectionTitle)
-  
-      
-  sectionPanier.appendChild(sectionTitre);
-for (let i in produitsAjoutPanier){
-  
-let produitsPanier =document.createElement('article');
-produitsPanier.class.Name = ' selectionne';
-
-let idProduitPanier = document.createElement('a');
-
-console.log('a')
-
-}
+  let table = document.createElement("table");
+  for (let i in produitsAjoutPanier) {
+    table.innerHTML +=
+      "<tr><td>" +
+      produitsAjoutPanier[i].name +
+      "</td><td><img src=" +
+      produitsAjoutPanier[i].imageUrl +
+      " width=100px></td></tr>";
       //prix, couleur
   }
- 
-  
-
+  sectionPanier.appendChild(table);
+}
 document.body.appendChild(sectionPanier);
 //ajouter le total (somme de l'ens des produits du panier)
 
 //Perspectives: supprimer un elt du panier
-
 //formulaire en dessous du panier
+
